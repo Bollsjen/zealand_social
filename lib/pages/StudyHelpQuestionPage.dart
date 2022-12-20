6413656fbe45d9c061a4fd117457062b22bf52ed
Bollsjen <55839056+Bollsjen@users.noreply.github.com>
2022-12-20 02:55:08 +0100

    hhh

--- lib/pages/StudyHelpQuestionPage.dart
+++ lib/pages/StudyHelpQuestionPage.dart
@@ -21,28 +21,123 @@ class StudyHelpQuestionPage extends StatelessWidget {
 
             Expanded(
                 child: SingleChildScrollView(
-                  child: Container(
-                    child: Padding(
-                      padding: EdgeInsets.symmetric(),
-                      child: Column(
-                        crossAxisAlignment: CrossAxisAlignment.start,
-                        children: [
-                          Padding(
-                            padding: EdgeInsets.symmetric(),
+                  child: Column(
+                    crossAxisAlignment: CrossAxisAlignment.start,
+                    children: [
+                      Padding(
+                        padding: EdgeInsets.symmetric(vertical: 12, horizontal: 8),
+                        child: Container(
+                          child: Align(
+                            alignment: Alignment.centerLeft,
                             child: Text(
-                                'This is a question',
-                                style: TextStyle(
-                                    fontSize: 22,
-                                    fontWeight: FontWeight.bold
-                                )
+                              'This is a question title',
+                              style: TextStyle(
+                                  fontSize: 22,
+                                  fontWeight: FontWeight.bold
+                              ),
                             ),
-                          )
-                        ],
+                          ),
+                        ),
                       ),
-                    ),
+
+                      Container(
+                        decoration: BoxDecoration(
+                          border: Border(bottom: BorderSide(color: Colors.grey.shade400))
+                        ),
+                        child: Padding(
+                          padding: EdgeInsets.only(left: 8, right: 8, bottom: 8),
+                          child: Row(
+                            crossAxisAlignment: CrossAxisAlignment.start,
+                            children: [
+                              Padding(
+                                  padding: EdgeInsets.only(right: 12),
+                                child: Wrap(
+                                  children: [
+                                    Text(
+                                      'Asked ',
+                                      style: TextStyle(
+                                          fontSize: 14,
+                                          color: Colors.grey.shade600
+                                      ),
+                                    ),
+
+                                    Text(
+                                      'today',
+                                      style: TextStyle(
+                                          fontSize: 14
+                                      ),
+                                    ),
+                                  ],
+                                ),
+                              ),
+
+                              Padding(
+                                padding: EdgeInsets.only(right: 12),
+                                child: Wrap(
+                                  children: [
+                                    Text(
+                                      'Viewd ',
+                                      style: TextStyle(
+                                          fontSize: 14,
+                                          color: Colors.grey.shade600
+                                      ),
+                                    ),
+
+                                    Text(
+                                      '12 times',
+                                      style: TextStyle(
+                                          fontSize: 14
+                                      ),
+                                    ),
+                                  ],
+                                ),
+                              ),
+                            ],
+                          ),
+                        ),
+                      ),
+
+                      Container(
+                        child: Row(
+                          children: [
+                            Container(
+                              child: Column(
+                                children: [
+                                  Align(
+                                    alignment: Alignment.centerLeft,
+                                    child: Padding(
+                                      padding: EdgeInsets.symmetric(horizontal: 0,),
+                                      child: IconButton(
+                                        padding: EdgeInsets.zero,
+                                        onPressed: () {},
+                                        icon: Icon(
+                                          Icons.arrow_drop_up_outlined,
+                                          size: 62,
+                                        ),
+                                      ),
+                                    ),
+                                  ),
+
+                                  Padding(
+                                    padding: EdgeInsets.symmetric(horizontal: 24),
+                                    child: Text(
+                                      '0',
+                                      style: TextStyle(
+                                        fontSize: 22,
+                                        color: Colors.grey.shade600
+                                      )
+                                    ),
+                                  )
+                                ],
+                              ),
+                            )
+                          ],
+                        ),
+                      )
+                    ],
                   ),
                 ),
-            )
+            ),
           ],
         ),
       ),
